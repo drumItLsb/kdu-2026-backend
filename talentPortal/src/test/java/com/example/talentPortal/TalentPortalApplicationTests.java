1988642cc12dc861eb8879d44c3232aadbb90211
drumItLsb <likhit.b@kickdrumtech.com>
2026-01-11 21:04:10 +0530

    setup project

---
+++ talentPortal/src/test/java/com/example/talentPortal/TalentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talentPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
