74956e7db75aac32dcb889ab2608257e4c322bca
drumItLsb <likhit.b@kickdrumtech.com>
2026-01-12 13:01:25 +0530

    add executor service

---
+++ logistics/src/main/java/com/example/logistics/config/ExecutorConfig.java
@@ -0,0 +1,15 @@
+package com.example.logistics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class ExecutorConfig {
+    @Bean
+    public ExecutorService executorService() {
+        return Executors.newFixedThreadPool(5); // Max 5 threads
+    }
+}
--- logistics/src/main/java/com/example/logistics/services/PackageService.java
+++ logistics/src/main/java/com/example/logistics/services/PackageService.java
@@ -7,21 +7,37 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+
 @Service
 public class PackageService {
     private final PackageRepository packageRepository;
+    private final ExecutorService executorService;
 
-    public PackageService(PackageRepository packageRepository) {
+    public PackageService(PackageRepository packageRepository, ExecutorService executorService) {
         this.packageRepository = packageRepository;
+        this.executorService = executorService;
     }
 
-    public Package addPackage(Package book) {
+    public Package addPackage(Package pack) {
         try {
-            return packageRepository.save(book);
+            Package p = packageRepository.save(pack);
+            executorService.submit(() -> processPackageAddition(p.getId()));
+            return p;
         } catch(DataIntegrityViolationException e) {
             // DataIntegrityViolationException: indicates database error like not unique, adding null
             throw new RuntimeException("Database error: "+ e.getMessage());
         }
+    }
+
+    public void processPackageAddition(String id) {
+        try {
+            Thread.sleep(3000);
+            Package p = packageRepository
+                    .findById(id).orElseThrow(() -> new RuntimeException("package not found"));
+            p.setStatus("SORTED");
+            packageRepository.save(p);
+        } catch (InterruptedException e) {}
     }
 
     public int findTotalRevenue() {
