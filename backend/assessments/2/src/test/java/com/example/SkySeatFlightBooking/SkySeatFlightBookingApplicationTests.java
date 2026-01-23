a30fa6ec789b3058b0be1d9e2efcf66dd7ae6d29
drumItLsb <likhit.b@kickdrumtech.com>
2026-01-23 11:55:01 +0530

    initialize project

---
+++ backend/assessments/2/src/test/java/com/example/SkySeatFlightBooking/SkySeatFlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkySeatFlightBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySeatFlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
